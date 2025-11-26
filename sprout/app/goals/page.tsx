9d28b3722a258478e6ab5d326752f98ec7a92efe
bNTGeez <tangbenjamin123@gmail.com>
2025-11-25 22:59:50 -0800

    Restructure repo: frontend in sprout/, add backend scaffold

---
+++ sprout/app/goals/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Card from '@/app/components/Card'
+
+const page = () => {
+  return (
+    <div className='min-h-screen bg-gray-50'>
+      <div >
+        <h1 className='mx-4 my-6 text-2xl font-bold text-gray-900'>Goals</h1>
+      </div>
+      <div className=''>
+        <Card >
+          <h2>Active Goals</h2>
+        </Card>
+      </div>
+    </div>
+  )
+}
+
+export default page
